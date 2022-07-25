3443ce5ee992e9b255da5a148e047af2b8175713
Vasyl Petryna <vasylpetryna2002gmail.com>
2022-07-25 11:34:23 +0300

    feat: add mock data for header and links

---
+++ src/__tests__/__mock__/mock-header.ts
@@ -0,0 +1,9 @@
+import {HeaderType} from "../../models";
+import {mockLinks} from "./mock-links";
+
+export const mockHeader: HeaderType = {
+    logo: {
+        url: "logo.jpg"
+    },
+    navigation: mockLinks
+}

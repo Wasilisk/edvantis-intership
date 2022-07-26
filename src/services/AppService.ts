760aa1cc7d9cdf3ffe523d63816b6f68a483f09c
Vasyl Petryna <vasylpetryna2002gmail.com>
2022-07-26 19:53:37 +0300

    feat: add a method to get data about the home page

--- src/services/AppService.ts
+++ src/services/AppService.ts
@@ -1,10 +1,14 @@
 import axios, {AxiosResponse} from 'axios';
-import {LayoutType} from "../models";
+import {HomePageType, LayoutType} from "../models";
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 export class AppService {
     static async getAppLayout(): Promise<AxiosResponse<LayoutType>> {
         return axios.get('layout');
+    }
+
+    static async getHomePageData(): Promise<AxiosResponse<HomePageType>> {
+        return axios.get('home_page');
     }
 }

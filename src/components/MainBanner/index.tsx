a7f8a5c664c2dc454ffe55d4fbe59e9bb35955ef
Vasyl Petryna <vasylpetryna2002gmail.com>
2022-07-26 19:56:20 +0300

    feat: add main banner component

---
+++ src/components/MainBanner/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {MainBannerStyles} from './styles';
+import {useAppSelector} from "../../hooks";
+import {selectMainBanner} from "../../store/slices/HomePage/selectors";
+import LinkButton from "../common/LinkButton";
+
+const MainBanner = () => {
+    const mainBannerData = useAppSelector(selectMainBanner);
+
+    return (
+        mainBannerData && <MainBannerStyles backgroundImage={mainBannerData.image.url}>
+            <div className="banner-info">
+                <h1>
+                    <span className="highlight">{mainBannerData.highlight}&nbsp;</span>
+                    {mainBannerData.plain_title}
+                </h1>
+                <p>{mainBannerData.description}</p>
+                <LinkButton
+                    data-testid="link-button"
+                    to={mainBannerData.link.to}
+                    variant="filled"
+                    size="large"
+                    label={mainBannerData.link.name}
+                    hasArrow
+                    fullWidth={false}
+                />
+            </div>
+            <div className="banner-image" data-testid="banner"/>
+        </MainBannerStyles>
+    );
+};
+
+export default MainBanner;
---
+++ src/components/MainBanner/types.ts
@@ -0,0 +1,3 @@
+export type MainBannerStylesProps = {
+    backgroundImage: string
+}

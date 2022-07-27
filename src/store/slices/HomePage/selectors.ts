38dc316b0271592e2d684dd409b26c5c75a0218e
Vasyl Petryna <vasylpetryna2002gmail.com>
2022-07-27 16:23:53 +0300

    feat: add a selector for selecting promotions

--- src/store/slices/HomePage/selectors.ts
+++ src/store/slices/HomePage/selectors.ts
@@ -1,3 +1,4 @@
 import {RootState} from "../../index";
 
 export const selectMainBanner = (state: RootState) => state.home_page.main_banner;
+export const selectPromotions = (state: RootState) => state.home_page.promotions;

e7b702aa47070dfa4aa8c7e11b4d72b5637f4c58
Vasyl Petryna <vasylpetryna2002gmail.com>
2022-07-27 16:24:37 +0300

    feat: add mock promotions for tests

--- src/__tests__/__mock__/index.ts
+++ src/__tests__/__mock__/index.ts
@@ -1,2 +1,4 @@
 export * from './mock-links';
 export * from './mock-header';
+export * from './mock-main-banner';
+export * from './mock-promotions';
---
+++ src/__tests__/__mock__/mock-promotions.ts
@@ -0,0 +1,62 @@
+export const mockPromotions = [
+    {
+        id: 1,
+        image: {
+            url: "https://res2.weblium.site/res/5e2f1da2c947890021c3f194/5e334f7786cda400222d1564_optimized_1396?nowebp",
+            alt: "Modern Collection"
+        },
+        title: {
+            highlight: "NEW!",
+            plain_text: "Modern Collection"
+        },
+        link: {
+            name: "Learn more",
+            to: "/promotions"
+        }
+    },
+    {
+        id: 2,
+        image: {
+            url: "https://res2.weblium.site/res/5e2f1da2c947890021c3f194/5e334f7786cda400222d1564_optimized_1396?nowebp",
+            alt: "Modern Collection"
+        },
+        title: {
+            highlight: "NEW!",
+            plain_text: "Modern Collection"
+        },
+        link: {
+            name: "Learn more",
+            to: "#promotions"
+        }
+    },
+    {
+        id: 3,
+        image: {
+            url: "https://res2.weblium.site/res/5e2f1da2c947890021c3f194/5e334f7786cda400222d1564_optimized_1396?nowebp",
+            alt: "Modern Collection"
+        },
+        title: {
+            highlight: "NEW!",
+            plain_text: "Modern Collection"
+        },
+        link: {
+            name: "Learn more",
+            to: "#promotions"
+        }
+    },
+    {
+        id: 4,
+        image: {
+            url: "https://res2.weblium.site/res/5e2f1da2c947890021c3f194/5e334f7786cda400222d1564_optimized_1396?nowebp",
+            alt: "Modern Collection"
+        },
+        title: {
+            highlight: "NEW!",
+            plain_text: "Modern Collection"
+        },
+        link: {
+            name: "Learn more",
+            to: "#promotions"
+        }
+    }
+]

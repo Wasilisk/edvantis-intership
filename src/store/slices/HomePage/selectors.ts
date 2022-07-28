ab3a3dce8a20334cd95c36b8c071f802acd1393b
Vasyl Petryna <vasylpetryna2002gmail.com>
2022-07-28 13:36:16 +0300

    feat: add a selector for selecting goods selection data

--- src/store/slices/HomePage/selectors.ts
+++ src/store/slices/HomePage/selectors.ts
@@ -3,3 +3,4 @@ import {RootState} from "../../index";
 export const selectMainBanner = (state: RootState) => state.home_page.main_banner;
 export const selectPromotions = (state: RootState) => state.home_page.promotions;
 export const selectSectionBanner = (state: RootState) => state.home_page.section_banner;
+export const selectGoodsSelection = (state: RootState) => state.home_page.goods_selection;

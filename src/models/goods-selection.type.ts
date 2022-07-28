eb3173611a946f9cb072dd117e98fc496a97cba2
Vasyl Petryna <vasylpetryna2002gmail.com>
2022-07-28 13:34:27 +0300

    feat: add types for product card and goods selection

---
+++ src/models/goods-selection.type.ts
@@ -0,0 +1,7 @@
+import {ProductType} from "./product.type";
+
+export type GoodsSelectionType = {
+    title: string,
+    description: string,
+    products: ProductType[]
+}
---
+++ src/models/product.type.ts
@@ -0,0 +1,9 @@
+export type ProductType = {
+    id: number,
+    search_name: string,
+    name: string,
+    price: number
+    image: {
+        url: string
+    }
+}

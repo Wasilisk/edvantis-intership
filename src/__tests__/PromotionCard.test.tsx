d2ce900e61a586a8c416e23695d8b1afbc54415e
Vasyl Petryna <vasylpetryna2002gmail.com>
2022-07-28 13:31:16 +0300

    feat: update test for promotion card component

--- src/__tests__/PromotionCard.test.tsx
+++ src/__tests__/PromotionCard.test.tsx
@@ -17,16 +17,39 @@ describe('PromotionCard component', () => {
         const cardImage = getByAltText(mockPromotions[0].image.alt);
         expect(cardImage).toHaveAttribute('src', mockPromotions[0].image.url);
 
-        const highlightText = getByText(mockPromotions[0].title.highlight);
+        const highlightText = getByText(mockPromotions[0].title.highlight!);
         expect(highlightText).toBeDefined();
         expect(highlightText).toHaveClass(`highlight`);
 
-        const plainText = getByText(mockPromotions[0].title.highlight);
+        const plainText = getByText(mockPromotions[0].title.plain_text);
         expect(plainText).toBeDefined();
 
         const link = getByText(mockPromotions[0].link.name);
         fireEvent.click(link);
         expect(history.location.pathname).toBe(mockPromotions[0].link.to);
+
+        expect(asFragment()).toMatchSnapshot();
+    })
+    it('should render component without highlight text', () => {
+        const history = createMemoryHistory({ initialEntries: ['/'] });
+        const {queryByText, getByText, asFragment, getByAltText} = render(
+            <HistoryRouter history={history}>
+                <PromotionCard content={mockPromotions[2]}/>
+            </HistoryRouter>
+        );
+
+        const cardImage = getByAltText(mockPromotions[2].image.alt);
+        expect(cardImage).toHaveAttribute('src', mockPromotions[0].image.url);
+
+        const highlightText = queryByText("New!");
+        expect(highlightText).not.toBeInTheDocument();
+
+        const plainText = getByText(mockPromotions[2].title.plain_text);
+        expect(plainText).toBeDefined();
+
+        const link = getByText(mockPromotions[2].link.name);
+        fireEvent.click(link);
+        expect(history.location.pathname).toBe(mockPromotions[2].link.to);
 
         expect(asFragment()).toMatchSnapshot();
     })

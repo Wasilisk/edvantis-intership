6b9b6552ccf2390193216ae81281636cebd393cc
Vasyl Petryna <vasylpetryna2002gmail.com>
2022-07-29 20:43:38 +0300

    feat: add history wrapper for tests

---
+++ src/__tests__/__mock__/mock-styled-text.ts
@@ -0,0 +1,7 @@
+import {StyledTextType} from "../../models/common";
+
+export const mockStyledText: StyledTextType = {
+    before_highlight: "Text before",
+    highlight: "Highlight text",
+    after_highlight: "Text after"
+}

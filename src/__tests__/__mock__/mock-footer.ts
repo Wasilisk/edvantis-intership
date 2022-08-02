7f84fb2f1a42b2c8f6a7388c17a6dae03372f634
VasylPetryna <vasylpetryna2002@gmail.com>
2022-08-02 20:19:06 +0300

    feat: add mock footer data for tests

---
+++ src/__tests__/__mock__/mock-footer.ts
@@ -0,0 +1,15 @@
+import {mockLinks} from "./mock-links";
+
+export const mockFooter = {
+    logo: {
+        url: "https://res2.weblium.site/res/5e2f1da2c947890021c3f194/5e332770198fdb0021a86911_optimized_266?nowebp"
+    },
+    navigation: mockLinks,
+    "company_description": "We sell designer furniture, lamps & decor across the USA. We offer the best collections by American and European designers.",
+    "social_links": {
+        instagram: "https://www.instagram.com/",
+        facebook: "https://www.facebook.com/",
+        youtube: "https://www.youtube.com/"
+    },
+    created_by: "Owner"
+}

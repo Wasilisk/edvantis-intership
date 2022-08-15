1abf6a104bc3f554a6161f3a41e1610fd73168dc
VasylPetryna <vasylpetryna2002@gmail.com>
2022-08-15 17:55:42 +0300

    feat: add persisted store

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,22 @@ import App from './App';
 import {store} from "./store";
 import {Provider} from "react-redux";
 import {BrowserRouter} from "react-router-dom";
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 
+const persistedStore= persistStore(store);
+
 root.render(
     <React.StrictMode>
         <BrowserRouter>
             <Provider store={store}>
-                <App/>
+                <PersistGate loading={null} persistor={persistedStore}>
+                    <App/>
+                </PersistGate>
             </Provider>
         </BrowserRouter>
     </React.StrictMode>

5340df3beb1ca21ad3858e871f36448d90c8b10d
VasylPetryna <vasylpetryna2002@gmail.com>
2022-08-15 17:51:40 +0300

    feat: delete isLoading selector

--- src/store/slices/AppLayout/selectors.ts
+++ src/store/slices/AppLayout/selectors.ts
@@ -2,4 +2,3 @@ import {RootState} from "../../index";
 
 export const selectHeader = (state: RootState) => state.app_layout.header;
 export const selectFooter = (state: RootState) => state.app_layout.footer;
-export const selectIsLayoutLoading = (state: RootState) => state.app_layout.isLoading;

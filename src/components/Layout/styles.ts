97a77ba9a78c86b4c0c3321fbfefede293dc9c47
VasylPetryna <vasylpetryna2002@gmail.com>
2022-08-15 17:49:05 +0300

    styles: set position to relative

--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 export const LayoutStyles = styled.div`
+  position: relative;
+  
   .main-content {
     width: 100%;
     min-height: calc(100vh - 92px);

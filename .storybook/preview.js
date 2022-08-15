e2d41bc4f9e9740bf40e2805dd732a9c26e6f630
VasylPetryna <vasylpetryna2002@gmail.com>
2022-08-15 17:33:22 +0300

    feat: add redux provider to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import {GlobalStyle} from "../src/globalStyles";
 import {BrowserRouter} from "react-router-dom";
+import {configureStore} from "@reduxjs/toolkit";
+import {Provider} from "react-redux";
+import {rootReducer} from "../src/store";
+import 'react-phone-input-2/lib/style.css'
+
+const store = configureStore({
+    reducer: rootReducer
+});
 
 export const decorators = [
     (Story) => (
         <BrowserRouter>
-            <Story />
-            <GlobalStyle/>
+            <Provider store={store}>
+                <Story/>
+                <GlobalStyle/>
+            </Provider>
         </BrowserRouter>
     ),
 ];
